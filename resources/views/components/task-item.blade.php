686c1a2659350fb57650efc5b332f3fcf6bcdd71
PSAUM <83640023+PSAUM-dev@users.noreply.github.com>
2025-10-07 19:58:04 +0100

    Update task index page and task item component

---
+++ resources/views/components/task-item.blade.php
@@ -0,0 +1,12 @@
+<div class="col-12 col-xl-3 mt-3 mt-xl-0">
+    <div class="card">
+        <div class="card-body">
+            <p class="h6 card-title">Nom de la tâche</p>
+            <p class="text-muted text-sm">Description de la tâche ici</p>
+        </div>
+
+        <div class="bg-success p-2">
+            Terminée
+        </div>
+    </div>
+</div>
--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,5 +1,36 @@
+@props(['task' => 0])
+
 <x-base-layout>
 
-    La page des tasks ici
+    @if ($task == 1)
+
+        <div class="container">
+            <div class="row mt-xl-4">
+
+                @for ($i = 0; $i < 4; $i++)
+
+                    <x-task-item />
+
+                @endfor
+
+            </div>
+        </div>
+
+    @else
+
+        <div class="d-flex justify-content-center align-items-center h-90">
+            <div class="col-11 col-xl-6 p-4 border-1 border-secondary text-center">
+                <h3>Hello Dominique</h3>
+                <p>Commençons à organiser vos tâches sur TaSkello</p>
+                <a href="#" class="btn btn-outline-secondary">
+                    <div class="d-flex align-items-center">
+                        <box-icon name="plus" color="gray"></box-icon>
+                        <span>Commencer une nouvelle tâche</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    @endif
 
 </x-base-layout>
