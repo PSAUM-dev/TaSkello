910ebb2380e174bd20774deb845758783dba9e3a
PSAUM <83640023+PSAUM-dev@users.noreply.github.com>
2025-10-07 10:59:44 +0100

    Update Homepage texts

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -5,21 +5,25 @@
         <div class="row">
             <div class="col-12 col-xl-12">
 
-                <div class="p-10 col-12 col-xl-6">
-                    <p class="h3">Votre gestionnaire de tâches gratuit et open source</p>
-                    <p class="text-secondary">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur,
-                        nostrum saepe aliquid
-                        maxime voluptatibus a mollitia deserunt corporis voluptatum omnis beatae quod eos, voluptate
-                        dolores consequuntur assumenda deleniti aut officia.</p>
+                <div class="p-10 col-12 col-xl-9">
+                    <p class="h3">Organisez vos tâches, gagnez en clarté — avec TaSkello.</p>
+                    <p>Un gestionnaire de tâches simple, gratuit et open source, pensé pour les équipes modernes.</p>
+
+                    <p class="text-secondary">
+                        TaSkello vous aide à planifier, suivre et accomplir vos tâches plus facilement.
+                        Créez vos projets, suivez leur avancement et collaborez efficacement — tout cela depuis une
+                        interface claire et rapide.
+                        100 % open source, sans publicité, et accessible à tous.
+                    </p>
 
                     <div>
                         <a href="#" class="btn btn-outline-secondary">
                             <i class="bi bi-github"></i>
-                            <span>Github</span>
+                            <span>Découvrir sur Github</span>
                         </a>
                         <a href="#" class="btn btn-dark">
-                            <i class="bi bi-heart"></i>
-                            <span>Soutenir TaSkello</span>
+                            <i class="bi bi-heart text-danger-emphasis"></i>
+                            <span>Soutenir le projet</span>
                         </a>
                     </div>
                 </div>
@@ -33,17 +37,22 @@
         <div class="row">
 
             <div class="col-12 col-xl-6 p-4">
-                <p class="h4">Découvrir TaSkello</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, earum cum? Temporibus at rem eius
-                    nostrum tenetur</p>
-                <a href="#" class="btn btn-secondary">Découvrir</a>
+                <p class="h4">Pourquoi choisir TaSkello ?</p>
+                <p>
+                    Fini les outils trop complexes. TaSkello met la simplicité au cœur de la productivité.
+                    En quelques clics, créez, classez et suivez vos tâches, que ce soit pour un projet personnel ou en
+                    équipe.
+                </p>
+                <a href="#" class="btn btn-secondary">Découvrir les fonctionnalités</a>
             </div>
 
             <div class="col-12 col-xl-6 p-4 bg-body-tertiary">
-                <p class="h4">Gérer mes tâches</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, earum cum? Temporibus at rem eius
-                    nostrum tenetur</p>
-                <a href="#" class="btn btn-outline-secondary">Mes tâches</a>
+                <p class="h4">Gérez vos tâches comme un pro</p>
+                <p>
+                    Accédez à vos projets, ajoutez de nouvelles tâches et suivez leur progression.
+                    Grâce à une interface fluide et réactive, vous restez concentré sur l’essentiel.
+                </p>
+                <a href="#" class="btn btn-outline-secondary">Accéder à mes tâches</a>
 
 
             </div>
@@ -51,5 +60,5 @@
         </div>
     </div>
 
-    
+
 </x-base-layout>
