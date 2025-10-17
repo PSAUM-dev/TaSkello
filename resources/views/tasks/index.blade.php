189cc7aafed3b8d2be37bc91612d55dd00a9462c
PSAUM <83640023+PSAUM-dev@users.noreply.github.com>
2025-10-17 15:24:17 +0100

    Adding variable to store task count & updating main tag css class

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,5 +1,9 @@
 <x-base-layout>
 
+    @php
+        $tasksCount = count($tasks);
+    @endphp
+
     <div class="container mt-4">
         <div class="row justify-content-center">
 
@@ -7,50 +11,70 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-center mb-4">
-                            <p class="mb-4 h5">Tâches principales (<small class="text-muted h6">{{ count($tasks) }}</small>)</p>
+                            <p class="mb-4 h5">Tâches principales (<small
+                                    class="text-muted h6">{{ $tasksCount }}</small>)</p>
                             <x-new-task-button />
                         </div>
 
-                        <table class="table border-dark table-dark table-hover">
+                        @if ($tasksCount > 0)
 
-                            <tr>
-                                <th></th>
-                                <th>Nom</th>
-                                <th>Status</th>
-                                <th>Deadline</th>
-                            </tr>
+                            <table class="table border-dark table-dark table-hover">
 
-                            @foreach ($tasks as $task)
-
-                                @php
-
-                                    $taskStatus = [
-                                        'not_started' => 'En attentte',
-                                        'started' => 'Commencée',
-                                        'finished' => 'Terminée'
-                                    ];
-
-                                    $taskStatusColor = [
-                                        'not_started' => 'danger',
-                                        'started' => 'primary',
-                                        'finished' => 'success'
-                                    ];
-
-                                @endphp
                                 <tr>
-                                    <td><input class="form-check-input" type="checkbox" name="" id=""></td>
-
-                                    <td>
-                                        <a href="{{ route('tasks.show', $task) }}" class="link-light link-offset-3 link-underline-opacity-0 link-underline-opacity-50-hover">
-                                            {{ $task->name }}
-                                        </a>
-                                    </td>
-
-                                    <td><span class="badge text-bg-{{ $taskStatusColor[$task->status] }} p-2">{{ $taskStatus[$task->status] }}</span></td>
-                                    <td><span class="badge text-bg-secondary p-2">13/11/2025</span></td>
+                                    <th></th>
+                                    <th>Nom</th>
+                                    <th>Status</th>
+                                    <th></th>
                                 </tr>
-                            @endforeach
-                        </table>
+
+                                @foreach ($tasks as $task)
+
+                                    @php
+
+                                        $taskStatus = [
+                                            'not_started' => 'En attentte',
+                                            'started' => 'Commencée',
+                                            'finished' => 'Terminée'
+                                        ];
+
+                                        $taskStatusColor = [
+                                            'not_started' => 'danger',
+                                            'started' => 'primary',
+                                            'finished' => 'success'
+                                        ];
+
+                                    @endphp
+                                    <tr>
+                                        <td><input class="form-check-input" type="checkbox" name="" id=""></td>
+
+                                        <td>
+                                            <a href="{{ route('tasks.show', $task) }}"
+                                                class="link-light link-offset-3 link-underline-opacity-0 link-underline-opacity-50-hover">
+                                                {{ $task->name }}
+                                            </a>
+                                        </td>
+
+                                        <td><span
+                                                class="badge text-bg-{{ $taskStatusColor[$task->status] }} p-2">{{ $taskStatus[$task->status] }}</span>
+                                        </td>
+                                        <td class="d-flex align-items-center justify-content-between">
+                                            <a href="#"><i class="bi bi-pencil text-muted"></i></a>
+                                            <form action="{{ route('tasks.destroy', $task->id) }}" method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-outline-danger"><i
+                                                        class="bi bi-trash"></i></button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+
+                        @else
+                            <p class="text-center bg-body-tertiary p-3">Pas de tâches disponibles.</p>
+                        @endif
+
+
                     </div>
                 </div>
 
