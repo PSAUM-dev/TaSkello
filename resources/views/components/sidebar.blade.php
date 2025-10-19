5272fbfbbf494a4cd4e03b6e4dbf2f736d7456ec
PSAUM <83640023+PSAUM-dev@users.noreply.github.com>
2025-10-19 14:26:25 +0100

    Update sidebar menu behavior

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
             aria-labelledby="offcanvasDarkNavbarLabel">
 
-            <div class="offcanvas-header">
+            <div class="offcanvas-header bg-body-tertiary">
                 <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">TaSkello</h5>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
                     aria-label="Close"></button>
@@ -11,13 +11,18 @@
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
 
                     <li class="nav-item d-flex align-items-center">
-                        <box-icon name="menu" color="gray" size="1.2em"></box-icon>
-                        <a class="nav-link ml-3 active" href="{{ route('dashboard') }}">Dashboard</a>
+                        <i class="bi bi-kanban text-muted"></i>
+                        <a class="nav-link ml-3 {{ Route::is('dashboard') ? 'active' : '' }}" href="{{ route('dashboard') }}">Dashboard</a>
                     </li>
 
                     <li class="nav-item d-flex align-items-center">
-                        <box-icon name="clipboard" color="gray" size="1.2em"></box-icon>
-                        <a class="nav-link ml-3" href="{{ route('tasks.index') }}">Toutes mes tâches</a>
+                        <i class="bi bi-clipboard text-muted"></i>
+                        <a class="nav-link ml-3 {{ Route::is('tasks.*') ? 'active' : '' }}" href="{{ route('tasks.index') }}">Mes tâches</a>
+                    </li>
+
+                    <li class="nav-item d-flex align-items-center">
+                        <i class="bi bi-briefcase text-muted"></i>
+                        <a class="nav-link ml-3 {{ Route::is('projects') ? 'active' : '' }}" href="#">Mes projets</a>
                     </li>
 
                 </ul>
